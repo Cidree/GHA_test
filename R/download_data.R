dc20ad9259387a44907ff5afd48ce11ac4b92045
Cidree <adrian.cidre@gmail.com>
2024-06-25 21:50:32 +0200

    fix occ

--- R/download_data.R
+++ R/download_data.R
@@ -5,7 +5,7 @@ library(readr)
 library(sf)
 library(dplyr)
 
-## Search occurrences 2 2 2 2 2
+## Search occurrences
 cerambyx_po <- geodata::sp_occurrence(
     genus   = "Cerambyx",
     species = "cerdo",
